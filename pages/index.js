1c13020f571b93ba7fbb8fe54e7cff53985247e7
KT <kylebrandontan@gmail.com>
2022-06-09 23:41:49 +0800

    fetching items from the back room (ipfs)

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Product from '../components/Product';
+
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 import HeadComponent from '../components/Head';
+import { publicKey } from "@solana/web3.js";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fshk-images.s3.amazonaws.com%2Fwp-content%2Fuploads%2F2018%2F08%2Fbanana-with-googly-eyes-Large.jpeg&f=1&nofb=1" className="img" alt="banana" />      
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map(product => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
   
   return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header">🍌 Buy My Solana Banana 🍌</p>
+          <p className="sub-text">Don't you wanna buy my banana? 😳</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
